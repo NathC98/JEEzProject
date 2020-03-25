9d12cf7e6e0c810b488868aac5a1ce75772a7876
lleguen <louisleg29@gmail.com>
2020-03-26 00:33:55 +0100

    Création des classes Administrator, game, identifiant, play, player, player_has_game

---
+++ src/Identifiant.java
@@ -0,0 +1,32 @@
+package AppClasses;
+
+public class Identifiant {
+
+    private int idIdentifiant;
+    private String login;
+    private String password;
+
+    public int getIdIdentifiant() {
+        return idIdentifiant;
+    }
+
+    public void setIdIdentifiant(int idIdentifiant) {
+        this.idIdentifiant = idIdentifiant;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
