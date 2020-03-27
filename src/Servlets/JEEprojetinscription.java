ed6bb9da9a2f3b0c798d11d82fdf78f2770835c9
shasolver <pleroy@enssat.fr>
2020-03-27 05:20:54 +0100

    Reremodification de l'inscription

--- src/Servlets/JEEprojetinscription.java
+++ src/Servlets/JEEprojetinscription.java
@@ -6,7 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.*;
+import AppClasses.*;
+import java.util.*;
+import java.text.*;
 
 @WebServlet(name = "JEEprojetinscription")
 public class JEEprojetinscription extends HttpServlet {
